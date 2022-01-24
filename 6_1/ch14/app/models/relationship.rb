3543703f996985b24e606a1381d4fc7a3f1c7e9d
Takao Taira <takao.taira@gmail.com>
2022-01-24 20:40:41 +0900

    Impl recent_followers

--- 6_1/ch14/app/models/relationship.rb
+++ 6_1/ch14/app/models/relationship.rb
@@ -1,6 +1,13 @@
 class Relationship < ApplicationRecord
-  belongs_to :follower, class_name: "User"
-  belongs_to :followed, class_name: "User"
+  belongs_to :follower, class_name: 'User'
+  belongs_to :followed, class_name: 'User'
   validates :follower_id, presence: true
   validates :followed_id, presence: true
+
+  def self.recent_followers(followed_id, within_time)
+    relations = where(followed_id: followed_id).order(created_at: :desc)
+    relations.select do |relation|
+      relation.created_at >= (Time.current - within_time)
+    end.map(&:follower)
+  end
 end
